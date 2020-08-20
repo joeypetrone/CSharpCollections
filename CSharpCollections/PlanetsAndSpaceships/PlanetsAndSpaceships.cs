1504b49651376a64f6c4ace0536e2f659eac74fc
Joey Petrone <joey.petrone@gmail.com>
2020-08-19 21:12:13 -0500

    All planets in planet list

--- CSharpCollections/PlanetsAndSpaceships/PlanetsAndSpaceships.cs
+++ CSharpCollections/PlanetsAndSpaceships/PlanetsAndSpaceships.cs
@@ -12,7 +12,30 @@ namespace CSharpCollections.PlanetsAndSpaceships
         }
         public override void Run()
         {
-            Console.WriteLine("In Planets And Spaceships");
+            List<string> planetList = new List<string>() { "Mercury", "Mars" };
+
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> lastTwoPlanets = new List<string>() { "Uranus", "Neptune" };
+
+            planetList.AddRange(lastTwoPlanets);
+
+            planetList.Insert(1, "Venus");
+
+            planetList.Insert(2, "Earth");
+
+            planetList.Add("Pluto");
+
+            var rockyPlanets = planetList.GetRange(0, 4);
+
+            planetList.ForEach(Console.WriteLine);
+
+            Console.WriteLine("Rocky Planets:");
+
+            rockyPlanets.ForEach(Console.WriteLine);
+
+            planetList.Remove("Pluto");
         }
     }
 }
