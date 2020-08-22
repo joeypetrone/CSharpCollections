a482c9a87742c48fb0dcce2ab00bec1be163869e
Joey Petrone <joey.petrone@gmail.com>
2020-08-22 17:51:15 -0500

    Favorite Pizza Toppings complete

--- CSharpCollections/FavoritePizzaToppings/FavoritePizzaToppings.cs
+++ CSharpCollections/FavoritePizzaToppings/FavoritePizzaToppings.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
 
 namespace CSharpCollections.FavoritePizzaToppings
 {
@@ -12,7 +15,35 @@ namespace CSharpCollections.FavoritePizzaToppings
         }
         public override void Run()
         {
-            Console.WriteLine("In Favorite Pizza Toppings");
+            var json = File.ReadAllText("FavoritePizzaToppings/pizza.json");
+
+            var pizzas = JsonSerializer.Deserialize<List<Pizza>>(json);
+
+            var toppingsOrderedStringList = pizzas.Select(pizza => String.Join(", ", pizza.toppings.OrderBy(topping => topping)));
+
+            var toppingsDictionary = new Dictionary<string, int>();
+
+            foreach (var topping in toppingsOrderedStringList)
+            {
+                if (!toppingsDictionary.ContainsKey(topping))
+                {
+                    toppingsDictionary.Add(topping, 1);
+                }
+                else
+                {
+                    toppingsDictionary[topping]++;
+                }
+            }
+
+            var topTwentyToppings = toppingsDictionary.OrderByDescending(toppings => toppings.Value).ToList().GetRange(0, 20);
+
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+
+            foreach (var toppings in topTwentyToppings)
+            {
+                Console.WriteLine($"#{topTwentyToppings.IndexOf(toppings) + 1} Pizza Topping(s): {textInfo.ToTitleCase(toppings.Key)}\nOrder Count: {toppings.Value}\n");
+            }
+
         }
     }
 }
